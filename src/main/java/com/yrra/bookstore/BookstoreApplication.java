81f5ff3417dec9d4dfe0dab8de6f8bf2a2b54daf
Andres Yrra <yrraandres@gmail.com>
2017-12-05 21:08:48 -0800

    initial commit, configured for bootstrap

---
+++ src/main/java/com/yrra/bookstore/BookstoreApplication.java
@@ -0,0 +1,12 @@
+package com.yrra.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+}
